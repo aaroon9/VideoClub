d5f4a349dc585e37846bf8c248930159504a0cd1
aaroon9 <adrian.valera.garcia@gmail.com>
2019-02-07 21:14:08 +0100

    Inicio Proyecto
    Empezamos proyecto

---
+++ routes/web.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Llamar a un controlador, el @ indica que llama dentro del archivo a uno en concreto.
+//Route::get('/{id?}', 'MoviesController@showMovie');
+
+Route::get('/', 'HomeController@getHome');
+
+/*Route::get('/login', function () {
+    return view('auth.login');
+});
+
+Route::get('/logout', function () {
+    return view('logout');
+});*/
+
+Route::group(['middleware' => 'auth'], function() {
+	Route::get('/catalog', 'CatalogController@getIndex'); 
+
+	Route::get('/catalog/show/{id}', 'CatalogController@getShow');
+	    
+
+	Route::get('/catalog/create', 'CatalogController@getCreate');
+	    
+
+	Route::get('/catalog/edit/{id}', 'CatalogController@getEdit');
+
+	Route::post('/catalog/create', 'CatalogController@postCreate');
+	Route::put('/catalog/edit/{id}', 'CatalogController@putEdit');
+
+	Route::put('/catalog/rent/{id}', 'CatalogController@putRent');
+	Route::put('/catalog/return/{id}', 'CatalogController@putReturn');
+	Route::delete('/catalog/delete/{id}', 'CatalogController@deleteMovie');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
