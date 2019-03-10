68caea881b7baa924b7c2ee55635e0da9212412e
Xavier Franch <xavi.franchtarazaga@gmail.com>
2019-03-10 10:52:31 +0100

    Nuevo carrito

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -5,9 +5,11 @@ use Illuminate\Http\Request;
 use App\Alquiler;
 use App\Movie;
 use App\User;
+use App\Carrito;
 use Auth;
 use Notification;
-use Cart;
+use Illuminate\Support\Facades\DB;
+// use Cart;
 
 class CartController extends Controller
 {
@@ -16,13 +18,35 @@ class CartController extends Controller
     $movie = Movie::all();
     return view('payments.carrito', array('pelicula' => $movie));
   }
+
   public function createItem($id_movie){
+
+    $peli = Movie::findOrFail($id_movie);
+
+    // comprobamos si existe el articulo  en el carrito
+    $article = Carrito::findOrFail($id_movie);
+
+
+    if ($article){
+      // si existe anadimos una unidad
+        $article->increment('unidads',1);
+    } else {
+      // sino lo creamos
+        $article = new Carrito();
+        $article->unidads = 1;
+        $article->title = $peli->title;
+        $article->precio = $peli->precio;
+        $article->save();
+    }
+
+
 
      Cart::add($id_movie, '3', 1, 3.90);  //Afegir element
 
     Notification::success('Pelicula añadida al carrito');
     return redirect('/catalog/show/'.$id_movie);
   }
+
   public function destroyCart(){
     Cart::destroy();
     Notification::success('Carrito Vaciado');
