3b3b332718da1958b7d35bdce17412def79b716c
Xavier Franch <xavi.franchtarazaga@gmail.com>
2019-03-11 17:40:35 +0100

    Ara si que ya esta, cambios tontos

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -9,7 +9,7 @@ use App\Carrito;
 use Auth;
 use Notification;
 use Illuminate\Support\Facades\DB;
-// use Cart;
+use Cart;
 
 class CartController extends Controller
 {
@@ -18,35 +18,8 @@ class CartController extends Controller
     $movie = Movie::all();
     return view('payments.carrito', array('pelicula' => $movie));
   }
-<<<<<<< HEAD
-
-  public function createItem($id_movie){
-
-    $peli = Movie::findOrFail($id_movie);
-
-    // comprobamos si existe el articulo  en el carrito
-    $article = Carrito::findOrFail($id_movie);
-
-
-    if ($article){
-      // si existe anadimos una unidad
-        $article->increment('unidads',1);
-    } else {
-      // sino lo creamos
-        $article = new Carrito();
-        $article->unidads = 1;
-        $article->title = $peli->title;
-        $article->precio = $peli->precio;
-        $article->save();
-    }
-
-
-
-     Cart::add($id_movie, '3', 1, 3.90);  //Afegir element
-=======
 
   public function createItem($id_movie, Request $request){
->>>>>>> f46f8744dfc6630be0770b7faf3888cfa1322942
 
      $precio = $request->input('precio');
 
